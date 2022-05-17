07c1a83dbc1e8e70fcd5f3c58c84e7e3759a3d5a
Victor9578 <45564367+Victor9578@users.noreply.github.com>
2022-05-17 13:02:27 +0800

    Update xust_checkin.py

--- xust_checkin.py
+++ xust_checkin.py
@@ -62,18 +62,18 @@ def main_handler(event, context):
         'uid=RDEzRjI0OEFEOUFFNzEwRjlDMjhDNTVFMTMzOUEzNjc=',
         'uid=RkRBRDFDQkVFNTIzNDAxRjc1QkQ0QTRFOUJCNEYyNTA='
     )
-    payloadd = {"xkdjkdk":{"procinstid":"","empid":"76838","shzt":"-2","id":"","jrrq1":nt1,"sjh2":"13002982631","jrsfzx3":"是","szdd4":"中国陕西省西安市临潼区","xxdz41":"九号宿舍楼","jrtwfw5":"正常体温:36～37.2℃","jrsfjgwh6":"否","jrsfjghb7":"否","jrsfcxfrzz8":"否","jrsfywhrjc9":"否","jrsfyhbrjc10":"否","jrsfjcgrrq11":"否","jssfyqzysgl12":"否","sfcyglq13":"否","glkssj131":"","gljssj132":"","sfyyqxgzz14":"否","qtxx15":'',"gh":"18409040213","xm":"仵西龙","xb":"男","sfzh":"","szyx":"地质与环境学院","xydm":"4008","zy":"","zydm":"","bj":"地质工程1802班","bjdm":"4008024","jg":"","yx":"","sfxs":"是","xslx":"1","jingdu":"108.93285","weidu":"34.255","guo":"中国","sheng":"陕西省","shi":"西安市","xian":"临潼区","sfncxaswfx16":"否","dm":"4008024","jdlx":"0","tbsj":ntt,"fcjtgj17Qt":"","fcjtgj17":"","hqddlx":"1","ymtys":"","time":nt}}
+    payloadd = {"xkdjkdk":{"procinstid":"","empid":"76838","shzt":"-2","id":"","jrrq1":nt1,"sjh2":"13002982631","jrsfzx3":"是","szdd4":"中国陕西省西安市临潼区","xxdz41":"九号宿舍楼","jrtwfw5":"正常体温:36～37.2℃","jrsfjgwh6":"否","jrsfjghb7":"否","jrsfcxfrzz8":"否","jrsfywhrjc9":"否","jrsfyhbrjc10":"否","jrsfjcgrrq11":"否","jssfyqzysgl12":"否","sfcyglq13":"否","glkssj131":"","gljssj132":"","sfyyqxgzz14":"否","qtxx15":'',"gh":"18409040213","xm":"","xb":"男","sfzh":"","szyx":"地质与环境学院","xydm":"4008","zy":"","zydm":"","bj":"地质工程1802班","bjdm":"4008024","jg":"","yx":"","sfxs":"是","xslx":"1","jingdu":"108.93285","weidu":"34.255","guo":"中国","sheng":"陕西省","shi":"西安市","xian":"临潼区","sfncxaswfx16":"否","dm":"4008024","jdlx":"0","tbsj":ntt,"fcjtgj17Qt":"","fcjtgj17":"","hqddlx":"1","ymtys":"","time":nt}}
     xx = [
-       ['13002982631','18409040213','仵西龙','WuXiLong','陕西省','西安市','莲湖区','教场门77号'],
-       ['13891198746','18409040216','刘一凡','Fan','陕西省','延安市','子长市','张家沟小区'],
-       ['15719269901','18409040218','陈金龙','JiangShanXiaoYanYuYao','陕西省','汉中市','城固县','张骞路春熙花苑'],
-       ['18591310709','18409040215','位吉祥','varvel','陕西省','渭南市','临渭区','老城渭运小区'],
+       ['13002982631','18409040213','','WuXiLong','陕西省','西安市','莲湖区','教场门77号'],
+       ['13891198746','18409040216','','Fan','陕西省','延安市','子长市','张家沟小区'],
+       ['15719269901','18409040218','','JiangShanXiaoYanYuYao','陕西省','汉中市','城固县','张骞路春熙花苑'],
+       ['18591310709','18409040215','','varvel','陕西省','渭南市','临渭区','老城渭运小区'],
     ]
     for i in range(0,4):
         data = data9613[i]
         payloadd['xkdjkdk']['sjh2'] = xx[i][0]
         payloadd['xkdjkdk']['gh'] = xx[i][1]
-        payloadd['xkdjkdk']['xm'] = xx[i][2]
+        payloadd['xkdjkdk']['xm'] = NAME[i]
         msggg = xx[i][3]
         payload = payloadd
         print(payload)
